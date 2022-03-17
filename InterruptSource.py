28a66f62297462b35d8829f3fe9f3ace448d9c65
Oliver Maye <maye@ihp-ffo.de>
2022-03-17 12:50:01 +0100

    Minor change in InterruptSource
    [git-p4: depot-paths = "//depot/a_sy/shared/Python/philander/": change = 26429]

--- InterruptSource.py
+++ InterruptSource.py
@@ -17,10 +17,18 @@ class InterruptSource:
         if not self.eventEmitter is None:
             self.eventEmitter.off_all()
 
-    #    
     #
     # Gives the status of each interrupt condition, independently of whether
     # the condition is registered as a source of notification, or not.
     #
     def getIntStatus(self):
+        pass
+    
+    #
+    # Registers for interrupt notification.
+    # The mask specifies the interrupts of interest.
+    # The given handler will be called upon occurrence
+    # of the interrupts specified.
+    #
+    def registerInt(self, mask, handler=None ):
         pass
