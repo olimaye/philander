f13e8cd6a1d6e99dd8cebffebcc05beeb044e482
Oliver Maye <maye@ihp-ffo.de>
2023-05-17 10:18:32 +0100

    simdev: Finished documentation.
    [git-p4: depot-paths = "//depot/a_sy/shared/Python/philander/": change = 27770]

--- philander/simdev.py
+++ philander/simdev.py
@@ -1,6 +1,6 @@
-"""Serial bus simulation module to support debugging and cross-platform development.
+"""Serial device simulation module to support debugging and cross-platform development.
 
-This module provides a fake serial bus implementation to virtualize serial
+This module provides a fake serial device implementation to virtualize serial
 communication. 
 """
 __author__ = "Oliver Maye"
@@ -19,6 +19,18 @@ class SimDev():
     a single byte. Implementation should use as least as possible dependencies
     to other modules. Use of hardware-dependent drivers must be completely
     avoided!
+    
+    Objects of this type (and its derivatives) shall be stored as the
+    ``sim`` attribute of a matching :class:`serialbus.SerialDevice` object.
+    Remember that each :class:`serialbus.SerialDevice` must be registered
+    with a :class:`serialbus.SerialBus` by calling its :meth:`serialbus.SerialBus.attach`
+    method. The simulative serial bus implementation uses the
+    :attr:`serialbus.SerialDevice.deviceAddress` attribute to identify
+    the addressed device and then looks up its ``SerialBusDevice.sim``
+    attribute to retrieve the serial simulation for that device.
+    
+    For that reason, implementations do not have to care about the
+    address of the device - it's always the right one. 
     """
     
     def readByteRegister( self, aReg ):
@@ -51,9 +63,9 @@ class SimDev():
     def readWordRegister( self, aReg ):
         """Read a word from a certain register.
         
-        The word is formed from the content of the given register (low)
-        and the content of the immediate successor ``aReg+1`` of that
-        register (high).
+        The word is formed in little-endian order from the content of
+        the given register (low) and the content of the immediate
+        successor ``aReg+1`` of that register (high).
         
         :param int aReg: The address of the low-byte register to be read.
         :return: A 16-bit integer representing the response of the device\
@@ -68,13 +80,14 @@ class SimDev():
     def writeWordRegister( self, aReg, data16 ):
         """Write a double-byte (word) value into a certain register.
         
-        The method is expected to store the given value to a pair of
-        registers. The low-part of the data16 item is stored at the given
-        register, while the high-part is put at ``aReg+1``.
+        The method is expected to store the given value to a register or
+        pair of registers in little-endian order.
+        The low-part of the data16 item is stored at the given register,
+        while the high-part is put at ``aReg+1``.
         
-        :param int aReg: The address of the (low-) register to receive\
-        the low-part of the new value.
-        :param int data16: The new value to store to that pair of registers.
+        :param int aReg: The address of the register to receive the\
+        low-part of the new value.
+        :param int data16: The new value to store to that (pair of) registers.
         :return: An error code indicating success or the reason of failure.
         :rtype: ErrorCode
         """
@@ -87,9 +100,10 @@ class SimDev():
     def readDWordRegister( self, aReg ):
         """Read a double word from a certain register.
         
-        The dword is formed from the content of the four registers,
-        starting with the given address ``aReg`` (low-byte of the low-word)
-        and its successors ``aReg+1`` (high-byte of the low-word),
+        The dword is formed in little-endian order from the content of
+        the four registers, starting with the given address
+        ``aReg`` (low-byte of the low-word) and its successors
+        ``aReg+1`` (high-byte of the low-word),
         ``aReg+2`` (low-byte of the high-word) and
         ``aReg+3`` (high-byte of the high-word).
         
@@ -107,7 +121,8 @@ class SimDev():
         """Write a double-word (four bytes) value into a certain register.
         
         The method is expected to store the given value to a quadruple of
-        registers. The low-byte of the low word is stored at the given
+        registers in little-endian order.
+        The low-byte of the low word is stored at the given
         register ``aReg``. The high-byte of the low-word goes to ``aReg+1``.
         The low-part of the high-word is stored to ``aReg+2`` and the
         high-part of the high-word is put at ``aReg+3``.
@@ -128,7 +143,17 @@ class SimDev():
         """Read a block of data starting from the given register.
         
         Starting with the given Register address, ``length`` bytes are
-        read and returned.
+        read and returned. As with :meth:`readWordRegister` and
+        :meth:`readDWordRegister`, this implementation assumes an
+        auto-increment behavior of the target register. So, the returned
+        data buffer is read as follows:
+        
+            data[0] -> aReg
+            data[1] -> aReg + 1
+            ...
+
+        If this doesn't match the actual chip behavior, a sub-class should
+        overwrite this method.
         
         :param int aReg: The address of the first register to be read.
         :param int length: The number of bytes to read.
@@ -145,6 +170,8 @@ class SimDev():
     def writeBufferRegister( self, aReg, data ):
         """Write a block of byte data into registers.
         
+        As with :meth:`readBufferRegister` an auto-increment applies for
+        the target register.
         The first byte - at index zero - is stored at the given register
         ``aReg``, the next byte - at index 1 - is stored at ``aReg+1``
         and so on. More formally::
@@ -154,12 +181,14 @@ class SimDev():
             ...
 
         The number of bytes written is determined implicitly by the length
-        of the ``data`` list. 
+        of the ``data`` list.
+        If the auto-increment feature doesn't match the actual chip,
+        a sub-class should overwrite this method. 
         
         :param int aReg: The address of the first register to receive\
         the block of data.
         :param list data: List of bytes to be written. The length of the\
-        list determines the number of bytes to werite. So, all values in\
+        list determines the number of bytes to write. So, all values in\
         the list will be transferred to the device.
         :return: An error code indicating success or the reason of failure.
         :rtype: ErrorCode
@@ -170,7 +199,7 @@ class SimDev():
         return err
 
 class SimDevNull( SimDev, Module ):
-    """Slim line serial device simulation. Reading retrieves always the same\
+    """Slim-line serial device simulation. Reading retrieves always the same\
     constant value, while writing is simply ignored. 
     """
     
@@ -251,10 +280,12 @@ class MemoryType(enum.Enum):
     
 @dataclasses.dataclass
 class Register:
-    """Simulate a memory-based register. Depending on the type of memory,\
-    the register content can or cannot be changed by simply writing to it.\
-    Volatile registers may change their content spontaneously or by\
-    mechanisms that cannot be controlled by the user. 
+    """Simulate a memory-based register.
+    
+    Depending on the type of memory, the register content can or cannot
+    be changed by simply writing to it.
+    Volatile registers are not writable. They may change their content
+    spontaneously or by mechanisms that cannot be controlled by the user. 
     """
     address:    int
     """The address to identify this register during read/write operations."""
@@ -313,14 +344,15 @@ class SimDevMemory( SimDev ):
         return data, err
 
     def writeByteRegister( self, aReg, data ):
-        """Write a single byte value into a certain register.\
-        Write attempts to registers with non-writable memory are ignored.\
-        For registers with writable memory, the following sequence is\
-        executed in order to give sub-classes the opportunity to simulate\
+        """Write a single byte value into a certain register.
+        
+        Write attempts to registers with non-writable memory are ignored.
+        For registers with writable memory, the following sequence is
+        executed in order to give sub-classes the opportunity to simulate
         side effects:
         
         #. calling :meth:`._onPreWrite`, may alter the intended data and\
-        returns the actual new content.
+        returns the actual new content to write.
         #. writing the new register content
         #. calling :meth:`._onPostWrite`
         
@@ -344,14 +376,16 @@ class SimDevMemory( SimDev ):
 
     def _onPreRead(self, reg):
         """Interface function that will be called right before a register\
-        is read. Can be used by sub-classes to simulate the exact\
-        hardware behavior while reading a register. Modifying the
-        register content here, would highly affect the return value
-        of the surrounding :meth:`.readByteRegister` function.
+        is read.
+        
+        Can be used by sub-classes to simulate the exact hardware
+        behavior while reading a register. Modifying the register
+        content here, would highly affect the return value of the
+        surrounding :meth:`.readByteRegister` function.
         
         The return value is to indicate if the read operation will succeed.
         
-        The current implementation is simply empty.
+        This implementation is simply empty.
 
         :param Register reg: The register instance to be read.
         :return: An error code indicating success or the reason of failure.
@@ -362,13 +396,14 @@ class SimDevMemory( SimDev ):
     
     def _onPostRead(self, reg):
         """Interface function that will be called right after a register\
-        was read. Can be used by sub-classes to simulate the exact\
-        hardware behavior while reading a register.
-        Any action in this routine will not influence the return value
-        of the (current call of the) surrounding :meth:`.readByteRegister`
-        function.
+        was read.
         
-        The current implementation increments the register content if
+        Can be used by sub-classes to simulate the exact hardware
+        behavior while reading a register. Any action in this routine
+        will not influence the return value of the (current call of the)
+        surrounding :meth:`.readByteRegister` function.
+        
+        This implementation increments the register content if
         the register's memory type is :attr:`MemoryType.VOLATILE`.
 
         :param Register reg: The register instance to be read.
@@ -381,9 +416,11 @@ class SimDevMemory( SimDev ):
     
     def _onPreWrite(self, reg, newData):
         """Interface function that will be called right before a register\
-        is written. Can be used by sub-classes to simulate the exact\
-        hardware behavior while writing a register. The return value\
-        immediately defines the actual content to be written. 
+        is written.
+        
+        Can be used by sub-classes to simulate the exact hardware
+        behavior while writing a register. The return value immediately
+        defines the actual content to be written. 
         
         The current implementation just returns the `newData` argument.
 
@@ -399,12 +436,14 @@ class SimDevMemory( SimDev ):
     
     def _onPostWrite(self, reg):
         """Interface function that will be called right after a register\
-        was written. Can be used by sub-classes to simulate the exact\
-        hardware behavior while writing a register.
+        was written.
+        
+        Can be used by sub-classes to simulate the exact hardware
+        behavior while writing a register.
         
         The return value is to indicate if the write operation succeeded.        
         
-        The current implementation is simply empty.
+        This implementation is simply empty.
 
         :param Register reg: The register instance that was written.
         :return: An error code indicating success or the reason of failure.
