185f0b9383cf9db88994b22d929065ba831c6e96
Oliver Maye <maye@ihp-microelectronics.com>
2024-11-20 08:24:54 +0100

    Updated version info in preparation of the new release.

--- philander/__init__.py
+++ philander/__init__.py
@@ -1,7 +1,7 @@
 """A driver collection for embedded system components, such as sensors or battery chargers.
 """
 __author__ = "Oliver Maye"
-__version__ = "0.3.1"
+__version__ = "0.4"
 __all__ = ["accelerometer", "actuator",
            "battery", "ble", "bma456", "button",
            "charger", "configurable",
